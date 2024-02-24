5f7d91840a6e823812a87db7c22ca3b1611792dc
UrosJ2024 <161076289+UrosJ2024@users.noreply.github.com>
2024-02-24 19:27:15 +0100

    Update Notepad++.ps1

--- Notepad++.ps1
+++ Notepad++.ps1
@@ -1,11 +1,17 @@
-﻿# Postavljanje URL adrese Notepad++ MSI fajla
+# Postavljanje URL adrese Notepad++ MSI fajla
 $url = "https://github.com/UrosJ2024/Leanpay/blob/leanpay_test/Notepad%2B%2B7_9_1.msi"
 
-# Postavite putanju na kojoj želite da sačuvate preuzeti fajl na lokalnom računaru
+# Postavljanje putanje na kojoj ce biti sacuvan preuzeti fajl na lokalnom računaru
 $lokacija = "C:\Users\Public\Notepad++7_9_1.msi"
 
 # Preuzimanje fajla sa datog URL-a
-Invoke-WebRequest -Uri $url -OutFile $lokacija
+$response = Invoke-WebRequest -Uri $url -OutFile $lokacija
 
-# Pokretanje Notepad++ aplikacije na udaljenom računaru
-Start-Process msiexec.exe -ArgumentList "/i `"$lokacija`" /quiet /norestart" -Wait
+# Provera da li je preuzimanje uspešno
+if ($response.StatusCode -eq 200) {
+    Write-Host "Fajl uspešno preuzet."
+    # Pokretanje Notepad++ aplikacije na udaljenom računaru
+    Start-Process msiexec.exe -ArgumentList "/i `"$lokacija`" /quiet /norestart" -Wait
+} else {
+    Write-Host "Greška prilikom preuzimanja fajla."
+}
