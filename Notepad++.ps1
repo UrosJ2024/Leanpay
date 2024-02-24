c3e763842b81338d5440c5b212e5cd297665d20e
UrosJ2024 <161076289+UrosJ2024@users.noreply.github.com>
2024-02-24 19:13:26 +0100

    Add files via upload

---
+++ Notepad++.ps1
@@ -0,0 +1,11 @@
+﻿# Postavljanje URL adrese Notepad++ MSI fajla
+$url = "https://github.com/UrosJ2024/Leanpay/blob/leanpay_test/Notepad%2B%2B7_9_1.msi"
+
+# Postavite putanju na kojoj želite da sačuvate preuzeti fajl na lokalnom računaru
+$lokacija = "C:\Users\Public\Leanpay\Notepad++7_9_1.msi"
+
+# Preuzimanje fajla sa datog URL-a
+Invoke-WebRequest -Uri $url -OutFile $lokacija
+
+# Pokretanje Notepad++ aplikacije na udaljenom računaru
+Start-Process msiexec.exe -ArgumentList "/i `"$lokacija`" /quiet /norestart" -Wait
