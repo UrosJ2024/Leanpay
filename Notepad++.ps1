32935c29239527815fa5e82c184af766ef118bd5
UrosJ2024 <161076289+UrosJ2024@users.noreply.github.com>
2024-02-24 19:18:51 +0100

    Update Notepad++.ps1

--- Notepad++.ps1
+++ Notepad++.ps1
@@ -2,7 +2,7 @@
 $url = "https://github.com/UrosJ2024/Leanpay/blob/leanpay_test/Notepad%2B%2B7_9_1.msi"
 
 # Postavite putanju na kojoj želite da sačuvate preuzeti fajl na lokalnom računaru
-$lokacija = "C:\Users\Public\Leanpay\Notepad++7_9_1.msi"
+$lokacija = "C:\Users\Public\Notepad++7_9_1.msi"
 
 # Preuzimanje fajla sa datog URL-a
 Invoke-WebRequest -Uri $url -OutFile $lokacija
