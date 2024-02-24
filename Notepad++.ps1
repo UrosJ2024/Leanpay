edb7a65850109243176b7a0e5bb8b995961d00a3
UrosJ2024 <161076289+UrosJ2024@users.noreply.github.com>
2024-02-24 19:37:43 +0100

    Update Notepad++.ps1

--- Notepad++.ps1
+++ Notepad++.ps1
@@ -1,8 +1,17 @@
 # Postavljanje URL adrese Notepad++ MSI fajla
 $url = "https://github.com/UrosJ2024/Leanpay/blob/leanpay_test/Notepad%2B%2B7_9_1.msi"
 
-# Postavljanje putanje na kojoj ce biti sacuvan preuzeti fajl na lokalnom računaru
-$lokacija = "C:\Users\Public\Notepad++7_9_1.msi"
+# Postavljanje putanje na kojoj će biti sačuvan preuzeti fajl na lokalnom računaru
+$lokacija = "C:\Users\Public\Leanpay\Notepad++7_9_1.msi"
+
+# Postavljanje putanje za direktorijum "Leanpay"
+$direktorijum = "C:\Users\Public\Leanpay"
+
+# Provera da li direktorijum već postoji
+if (-not (Test-Path $direktorijum)) {
+    # Kreiranje direktorijuma ako ne postoji
+    New-Item -Path $direktorijum -ItemType Directory
+}
 
 # Preuzimanje fajla sa datog URL-a
 $response = Invoke-WebRequest -Uri $url -OutFile $lokacija
